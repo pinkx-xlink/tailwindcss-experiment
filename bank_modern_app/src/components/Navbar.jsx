50422cc98159e6a87f846d8fd061f458527a48e5
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-10-08 20:15:18 -0400

    Create navItems array; create template for Logos, Links, and Button - needs stylized

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const navItems = [
+    {name: "Home", href: '#'},
+    {name: "About", href: '#about'},
+    {name: "Company", href: '#company'},
+    {name: "Services", href: '#services'},
+    {name: "Contact", href: '#contact'},
+];
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen(isMenuOpen);
+
   return (
-    <div>Navbar</div>
+    <nav className='fixed'>
+        <div className=''>
+            {/* Logo */ }
+            <div className=''>
+                <div className=''>
+                    P
+                </div>
+                <span>
+                    ro 
+                    <span>
+                        Service
+                    </span>
+                </span>
+            </div>
+            {/* Links */}
+            <ul className=''>
+                {navItems.map(({name, href}) => (
+                    <li key={name}>
+                        <a 
+                        href=''
+                        className=''>
+                            {name}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            {/* Button */}
+            <div className=''>
+                <button>
+                    Get Started
+                </button>
+            </div>
+        </div>
+    </nav>
   )
 }
 
