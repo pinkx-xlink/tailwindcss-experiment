1a708f18cdd7341acd0be5215b220b26f8986289
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-10-09 16:41:48 -0400

    Begin building the About section; add the image

--- bank_modern_app/src/components/About.jsx
+++ bank_modern_app/src/components/About.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import about from '../assets/hands2.jpg'
 
 const About = () => {
   return (
-    <div>
-        
-    </div>
+    <section id='about' className='relative overflow-hidden bg-gradient-to-br
+    from-gray-50 to-green-50 py-12 px-4 sm:py-16 md:py-20 md:px-12
+    lg:px-20 flex flex-col lg:flex-row items-center justify-between'>
+        <div className=''>
+            <div className=''>
+                <img src={about} alt="About" />
+            </div>
+        </div>
+    </section>
   )
 }
 
