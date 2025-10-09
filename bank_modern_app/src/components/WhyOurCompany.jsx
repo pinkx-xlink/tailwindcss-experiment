a39eba10e63ee7824991a2b047e7188ba542a9dd
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-10-09 19:31:14 -0400

    Create the Why Our Company? div and its 2 card child elements

--- bank_modern_app/src/components/WhyOurCompany.jsx
+++ bank_modern_app/src/components/WhyOurCompany.jsx
@@ -1,8 +1,100 @@
 import React from 'react'
+import { Circle, Star, Zap } from 'lucide-react'
 
 const WhyOurCompany = () => {
   return (
-    <div>WhyOurCompany</div>
+    <section id='company'
+    className='relative overflow-hidden bg-gradient-to-br from-blue-50
+    to-purple-50 py-12 px-4 sm:py-16 md:py-12 lg:px-20'>
+        <div className='max-w-6xl mx-auto'>
+            <div 
+            className='text-center mb-6 md:mb-8'
+            data-aos='fade-down'>
+                <h2 className='text-3xl sm:text-4xl md:text-5xl
+                text-gray-900'>
+                    Why Our <span className='text-black font-bold'>Company</span>
+                    <span className='text-green-500'> ? </span>
+                </h2>
+                <div className='flex justify-center gap-3 mt-4'>
+                    <Circle className='text-pink-500 w-5 h-5'/>
+                    <Circle className='text-yellow-500 w-5 h-5'/>
+                    <Circle className='text-green-500 w-5 h-5'/>
+                </div>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-8
+                md:gap-12 item-center'>
+                    <div className='relative w-full flex justify-center
+                    order-2 lg:order-1'>
+                        <div
+                        className='w-full max-w-md lg:max-w-lg xl:max-w-xl
+                        h-[560px] md:h-[500px] lg:h-[520px] overflow-hidden
+                        bg-gray-50 shadow-lg md:shadow-xl absolute
+                        top-0 z-0 rounded-[30%_10%_20%_70%/60%_30%_70%_30%]'
+                        data-aos='fade-right'
+                        data-aos-delay='100'>
+                        </div>
+                <div className='relative z-10 p-4 sm:p-6 w-full max-w-md'>
+                    <div 
+                    className='flex items-start gap-4 sm:gap-6 p-4 transition-all
+                    bg-transparent'
+                    data-aos='fade-right'
+                    data-aos-delay='200'>
+                        <div className='flex-shrink-0 w-10 h-10 sm:w-12
+                        sm:h-12 flex items-center justify-center'>
+                            <Star className='w-5 h-5 sm:h-6 sm:w-6 md:w-7
+                            md:h-7 text-blue-600'/>
+                        </div>
+                        <div>
+                            <h3 className='text-lg sm:text-xl font-semibold
+                            text-gray-900 mb-2'>
+                                Excellence Guaranteed
+                            </h3>
+                            <p className='text-gray-600 text-sm sm:text-base'>
+                                Lorem ipsum paragraph
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div 
+                    className='flex items-start gap-4 sm:gap-6 p-4 transition-all
+                    bg-transparent'
+                    data-aos='fade-right'
+                    data-aos-delay='200'>
+                        <div className='flex-shrink-0 w-10 h-10 sm:w-12
+                        sm:h-12 flex items-center justify-center'>
+                            <Zap className='w-5 h-5 sm:h-6 sm:w-6 md:w-7
+                            md:h-7 text-pink-600'/>
+                        </div>
+                        <div>
+                            <h3 className='text-lg sm:text-xl font-semibold
+                            text-gray-900 mb-2'>
+                                Innovation Driven
+                            </h3>
+                            <p className='text-gray-600 text-sm sm:text-base'>
+                                Lorem ipsum paragraph
+                            </p>
+                            </div>
+                    </div>
+                    </div>
+                        </div>
+
+                        <div className='relative order-1 lg:order-2'>
+                            <div className='bg-gradient-to-br from-yellow-600 rounded-2xl
+                            md:rounded-3xl p-6 md:p-8 flex flex-col justify-center'
+                            data-aos='zoom-in'
+                            data-aos-delay='200'
+                            >
+                                <div className='max-w-md mx-auto text-center'>
+                                    
+                                </div>
+
+                            </div>
+                        </div>
+
+                        </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
