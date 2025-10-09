64c908bea49ff14f302b1c614108c9cadf6a2d5e
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-10-09 15:29:19 -0400

    Add top margin to Hero section

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import hero from '../assets/hands.jpg';
 
 const Hero = () => {
   return (
-    <section className='relative overflow-hidden bg-gray-50 py-12 px-4 
+    <section className='relative mt-6 overflow-hidden bg-gray-50 py-12 px-4 
     sm:px-6 md:px-8 lg:py-16 lg:px-20 flex flex-col lg:flex-row items-center
     justify-between'>
         <div className='flex-1 lg:ml-28 w-full max-w-xl space-y-6 relative
