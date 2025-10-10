cf4ba10cf70a1a31de7f1ea919842966c40a44ff
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-10-10 14:06:25 -0400

    Add the h2 and the 3 circles below it

--- bank_modern_app/src/components/Services.jsx
+++ bank_modern_app/src/components/Services.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Brush, Code, Camera } from 'lucide-react';
+import { Brush, Code, Camera, Circle } from 'lucide-react';
 
 const Services = () => {
     const [activeService, setActiveService] = useState(1);
@@ -40,18 +40,28 @@ const Services = () => {
     ];
 
   return (
-    <section id='service' className='relative'>
-        <div>
-            <div>
-                <div>
+    <section id='service' className='relative overflow-hidden
+    bg-gradient-to-br from-gray-50 to-green-50 py-12 px-4 sm:py-16
+    md:px-12 lg:px-20'>
+        <div className='max-w-7xl mx-auto'>
+            <div className='flex flex-col lg:flex-row items-center
+            justify-center text-center mb-6'
+            data-aos='fade-down'>
+                <div className='flex-1 max-w-2xl mx-auto space-y-6
+                mb-10 lg:mb-0'>
                     <div>
-                        <h2>
+                        <h2 className='text-3xl sm:text-4xl md:text-5xl
+                        text-gray-900'>
                             Our{" "}
-                            <span>
+                            <span className='font-bold text-black'>
                                 Services<span>.</span>
                             </span>
                         </h2>
-
+                        <div className='flex justify-center gap-3 mt-4'>
+                            <Circle className='text-pink-500 w-5 h-5'/>
+                            <Circle className='text-yellow-500 w-5 h-5'/>
+                            <Circle className='text-green-500 w-5 h-5'/>
+                        </div>
                     </div>
                 </div>
             </div>
