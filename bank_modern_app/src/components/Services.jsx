d10e21878ee4f3155fc23a43ae8e5f0f366bf569
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-10-10 13:23:27 -0400

    Create 2 more elements within the services array

--- bank_modern_app/src/components/Services.jsx
+++ bank_modern_app/src/components/Services.jsx
@@ -13,10 +13,32 @@ const Services = () => {
             color: "bg-pink-400",
             butColor: "bg-pink-400 hover:bg-pink-500",
             iconColor: "text-pink-100",
-            image: service1 
-        }
+            // image: service1
+        },
+            { 
+            id: 2,
+            icon: <Camera className='w-5 h-5 sm:w-6, sm:h-6'/>,
+            title: "Photography",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae risus massa. Donec maximus tortor id diam iaculis, commodo rhoncus mi volutpat. Ut aliquet, risus non volutpat tincidunt, nunc ligula tristique lacus, at consequat leo est et erat. Nam accumsan placerat hendrerit. In finibus pellentesque magna, nec mattis sapien feugiat vel. Nullam sed ultricies massa. In vel ornare dolor, eu scelerisque est.",
+            features: ["User Research", "Wireframing", "Prototyping", "UI Design"],
+            color: "bg-green-400",
+            butColor: "bg-green-400 hover:bg-green-500",
+            iconColor: "text-green-100",
+            // image: service2
+        },
+            { 
+            id: 3,
+            icon: <Code className='w-5 h-5 sm:w-6, sm:h-6'/>,
+            title: "Web Development",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae risus massa. Donec maximus tortor id diam iaculis, commodo rhoncus mi volutpat. Ut aliquet, risus non volutpat tincidunt, nunc ligula tristique lacus, at consequat leo est et erat. Nam accumsan placerat hendrerit. In finibus pellentesque magna, nec mattis sapien feugiat vel. Nullam sed ultricies massa. In vel ornare dolor, eu scelerisque est.",
+            features: ["User Research", "Wireframing", "Prototyping", "UI Design"],
+            color: "bg-yellow-400",
+            butColor: "bg-yellow-400 hover:bg-yellow-500",
+            iconColor: "text-yellow-100",
+            // image: service3
+        },
     ];
-    
+
   return (
     <div>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae risus massa. Donec maximus tortor id diam iaculis, commodo rhoncus mi volutpat. Ut aliquet, risus non volutpat tincidunt, nunc ligula tristique lacus, at consequat leo est et erat. Nam accumsan placerat hendrerit. In finibus pellentesque magna, nec mattis sapien feugiat vel. Nullam sed ultricies massa. In vel ornare dolor, eu scelerisque est.
