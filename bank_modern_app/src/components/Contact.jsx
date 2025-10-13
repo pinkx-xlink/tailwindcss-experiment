8f06432a5e5ee468799b354e0706f8a6219b5a77
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-10-13 13:05:15 -0400

    Create formFeilds array, which contains name, email, and message objects

--- bank_modern_app/src/components/Contact.jsx
+++ bank_modern_app/src/components/Contact.jsx
@@ -2,35 +2,61 @@ import { Mail, Phone, MapPinIcon } from 'lucide-react'
 import React from 'react'
 
 const Contact = () => {
-const contactInfo = [
-  {
-    id: 1, 
-    icon: Mail,
-    iconBg: 'bg-pink-100',
-    iconColor: 'text-pink-600',
-    title: 'Email',
-    content: 'info@example.com',
-    aosDelay: '200'
-  },
-  {
-    id: 2, 
-    icon: Phone,
-    iconBg: 'bg-yellow-100',
-    iconColor: 'text-yellow-600',
-    title: 'Phone Number',
-    content: '+123 456-7890',
-    aosDelay: '250'
-  },
-  {
-    id: 3, 
-    icon: MapPinIcon,
-    iconBg: 'bg-green-100',
-    iconColor: 'text-green-600',
-    title: 'Address',
-    content: '444 Main Street, City, Country',
-    aosDelay: '300'
-  },
-]
+  const contactInfo = [
+    {
+      id: 1, 
+      icon: Mail,
+      iconBg: 'bg-pink-100',
+      iconColor: 'text-pink-600',
+      title: 'Email',
+      content: 'info@example.com',
+      aosDelay: '200'
+    },
+    {
+      id: 2, 
+      icon: Phone,
+      iconBg: 'bg-yellow-100',
+      iconColor: 'text-yellow-600',
+      title: 'Phone Number',
+      content: '+123 456-7890',
+      aosDelay: '250'
+    },
+    {
+      id: 3, 
+      icon: MapPinIcon,
+      iconBg: 'bg-green-100',
+      iconColor: 'text-green-600',
+      title: 'Address',
+      content: '444 Main Street, City, Country',
+      aosDelay: '300'
+    },
+  ];
+
+  const formFeilds = [
+    {
+      id: 'name',
+      type: 'text',
+      label: 'Full Name',
+      placeholder: 'Enter your full name',
+      delay: '150'
+    },
+     {
+      id: 'email',
+      type: 'email',
+      label: 'Email Address',
+      placeholder: 'Enter your email address',
+      delay: '200'
+    },
+     {
+      id: 'message',
+      type: 'textarea',
+      label: 'Message',
+      placeholder: 'Enter your message here...',
+      rows: 5,
+      delay: '250'
+    },
+  ];
+
   return (
     <div>
 
