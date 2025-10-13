a0bb77b7b6e347b1022264a6a7d4ea0e4b12f9a8
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-10-13 15:54:08 -0400

    Create socialLinks array and import the social media logo icons from Lucide React; create quickLinks array

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { Facebook, Twitter, Instagram, Linkedin } from 'lucide-react';
 
 const Footer = () => {
+  const socialLinks = [
+    {icon: Facebook, href: '#', color:"hover:bg-pink-600"},
+    {icon: Twitter, href: '#', color:"hover:bg-blue-500"},
+    {icon: Instagram, href: '#', color:"hover:bg-pink-500"},
+    {icon: Linkedin, href: '#', color:"hover:bg-blue-600"},
+  ];
+
+  const quickLinks = [
+    {label: "Home", href:"#"},
+    {label: "About Us", href:"#"},
+    {label: "Company", href:"#"},
+    {label: "Services", href:"#"},
+    {label: "Contact", href:"#"},
+  ];
+
   return (
     <div>Footer</div>
   )
